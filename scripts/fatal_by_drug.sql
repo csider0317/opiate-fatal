d5957a1f9e028f152169a25e19291e17b31694a6
csider <csider0317@gmail.com>
2022-12-13 21:17:39 -0600

    updates

--- scripts/fatal_by_drug.sql
+++ scripts/fatal_by_drug.sql
@@ -1,21 +1,37 @@
 use opiate
 
-with fatal1 as (select drug_type, 
-sum(Value) as total_OD, 
-year
-from fatal
-WHERE  drug_type NOT LIKE '%All_%' AND value_type = 'Count'
-group by drug_type, year)
+SELECT * FROM fatal
+-- fatal od by drugs and year 
 select 
-total_OD, 
-year, 
-drug_type,
-row_number() over (
-    partition by Year(total_od)
-    order by total_OD desc
-  ) as top_number
-  FROM fatal1
+drug_type, 
+sum(value) as total_deaths,
+year
+FROM fatal
+WHERE drug_type NOT LIKE '%All%' and value_type = 'Count'
+group by year, drug_type
+order by total_deaths DESC
 
+-- non fatal od by drugs and year 
+select * from non_fatal_clean1
+
+select 
+drug_type, 
+sum(total_non_fatal) as total_od,
+year
+FROM non_fatal_clean1
+WHERE drug_type NOT LIKE '%All%' and value_type = 'Count' 
+group by year, drug_type
+order by total_od DESC
+
+-- fatal od by county and drug 
+select 
+Geography,
+drug_type, 
+sum(value) as total_deaths,
+year
+FROM fatal
+WHERE drug_type NOT LIKE '%All%' AND GeographyType = 'County' AND value_type = 'Count'
+group by year, drug_type, Geography
+order by total_deaths DESC
 
   
-select * from fatal 
